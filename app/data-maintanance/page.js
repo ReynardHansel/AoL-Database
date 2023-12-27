273b8b24ddf9b67892649edab0aabc5f0532f584
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2023-12-27 19:50:53 +0700

    Added CRUD page (empty)

--- app/data-maintanance/page.js
+++ app/data-maintanance/page.js
@@ -1,14 +1,15 @@
 export default function DataMaintanance() {
   return (
-<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 h-screen p-10">
-    <div className="card">
-        Sailors
-    </div>
-    <div className="card">
-        Boats
-    </div>
-    <div className="card col-span-1 lg:col-span-2">
-        Reserves
-    </div>
-</div>  )
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 h-screen p-10">
+        <div className="card">
+            Sailors
+        </div>
+        <div className="card">
+            Boats
+        </div>
+        <div className="card col-span-1 lg:col-span-2">
+            Reserves
+        </div>
+    </div>  
+)
 }
