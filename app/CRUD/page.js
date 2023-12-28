986aae9592b5c99b87bca3054c99e4c1530b5d73
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2023-12-28 07:52:01 +0700

    CRUD page improvement
    - CRUD page.js cards built
    - data-maintanance divs changed to Link
    - Added notes.txt

--- app/CRUD/page.js
+++ app/CRUD/page.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import Link from 'next/link'
 
-export default function Crud() {
+export default function Crud({ searchParams }) {
   return (
-    <div>
-      CRUD Page
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-screen p-10">
+        <Link href="#" className="card">Select</Link>
+        <Link href="#" className="card">Insert</Link>
+        <Link href="#" className="card">Delete</Link>
+        <Link href="#" className="card">Update</Link>
     </div>
   )
 }
+
--- app/data-maintanance/page.js
+++ app/data-maintanance/page.js
@@ -1,15 +1,40 @@
+import Link from 'next/link'
+
 export default function DataMaintanance() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 h-screen p-10">
-        <div className="card">
+        <Link className="card" 
+        href={{
+            pathname: "../CRUD",
+            query: {
+                value: 'sailor'
+            }
+        }}
+        >
             Sailors
-        </div>
-        <div className="card">
+        </Link>
+        
+        <Link className="card"
+        href={{
+            pathname:"../CRUD",
+            query: {
+                value:'boat'
+            }
+        }}
+        >
             Boats
-        </div>
-        <div className="card col-span-1 lg:col-span-2">
+        </Link>
+
+        <Link className="card col-span-1 lg:col-span-2"
+        href={{
+            pathname:"../CRUD",
+            query: {
+                value:'reserve'
+            }
+        }}
+        >
             Reserves
-        </div>
+        </Link>
     </div>  
 )
 }
