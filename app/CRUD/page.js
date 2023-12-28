6711d046cbd9c5bb33629d1223cad054d5021f34
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2023-12-28 09:02:55 +0700

    CRUD Content built
    - Made Select, Insert. Delete, Update pages (CRUD components)
    - Data (sailor/boat/reserve) successfully passed to CRUD components

--- app/CRUD/page.js
+++ app/CRUD/page.js
@@ -1,12 +1,52 @@
 import Link from 'next/link'
 
 export default function Crud({ searchParams }) {
+  // console.log(searchParams)
+  //* searchParams.value is what the user selects (sailor/boat/reserve)
+
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 gap-6 h-screen p-10">
-        <Link href="#" className="card">Select</Link>
-        <Link href="#" className="card">Insert</Link>
-        <Link href="#" className="card">Delete</Link>
-        <Link href="#" className="card">Update</Link>
+        <Link className="card"
+          href={{
+            pathname: "../CRUD/select",
+            query: {
+                value: searchParams.value
+            }
+        }}>
+          Select
+        </Link>
+
+        <Link className="card"
+          href={{
+            pathname: "../CRUD/insert",
+            query: {
+                value: searchParams.value
+            }
+        }}>
+          Insert
+        </Link>
+
+        <Link className="card"
+          href={{
+            pathname: "../CRUD/delete",
+            query: {
+                value: searchParams.value
+            }
+        }}>
+          Delete
+        </Link>
+
+        <Link className="card"
+          href={{
+            pathname: "../CRUD/update",
+            query: {
+                value: searchParams.value
+            }
+        }}>
+          Update
+        </Link>
+
+        {/* <p>{searchParams.value}</p> */}
     </div>
   )
 }
---
+++ app/CRUD/select/page.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Select({ searchParams }) {
+  return (
+    <div>
+        Select page
+      <p>{searchParams.value}</p>
+    </div>
+  )
+}
