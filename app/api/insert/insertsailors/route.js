78f23b18857e8682a0e16e499ae623c47315f07b
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2024-01-04 23:24:57 +0700

    Working insertsailors POST api, somehow...

--- app/api/insert/insertsailors/route.js
+++ app/api/insert/insertsailors/route.js
@@ -1,23 +1,44 @@
 import { db } from "../../../../database/server";
 import { NextResponse } from "next/server";
 
-export async function POST(req, res) {
+export async function POST(request) {
   try {
-    const { sid, sname, rating, age } = req.body;
+    const { sname, rating, age } = await request.json();
 
-    const sql = `INSERT INTO sailors (sid, sname, rating, age)
-                 VALUES (?, ?, ?, ?)`;
+    const sql = `INSERT INTO sailors (sname, rating, age) VALUES ('${sname}', ${rating}, ${age})`;
 
     const data = await new Promise((resolve, reject) => {
-      db.query(sql, [sid, sname, rating, age], (err, result) => {
+      db.query(sql, (err, result) => {
         if (err) return reject(err);
         resolve(result);
       });
     });
 
-    res.status(201).json({ message: "Sailor inserted successfully" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Error inserting sailor" });
+    console.log(data)
+
+    let json_response = {
+      status: "success",
+      data: {
+        sailor: { sname, rating, age },
+      },
+    };
+
+    return new NextResponse(JSON.stringify(json_response), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    let error_response = {
+      status: "error",
+      message: error.message,
+    };
+
+    return new NextResponse(JSON.stringify(error_response), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
+
+//! I don't know how tf that code actually works 💀
+//? Reference: https://codevoweb.com/build-a-simple-api-in-next-js-13-app-directory/
