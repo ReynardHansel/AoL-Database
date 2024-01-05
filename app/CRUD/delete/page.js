ab72294b4ca2f075743f2b8e69bdaff2590e380a
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2024-01-05 23:47:16 +0700

    Delete section fe-be connected for sailors, plus some notes and explanation at the bottom of /delete

--- app/CRUD/delete/page.js
+++ app/CRUD/delete/page.js
@@ -1,18 +1,83 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function Delete({ searchParams }) {
+  const [sailorID, setSailorID] = useState("");
+
+  //*Triggered when user inputs something
+  const handleChange = (e, setStateFunc) => {
+    setStateFunc(e.target.value);
+  };
+
+  //* Triggered when clicking submit button
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //? Variables to be submitted as JSON object to backend
+    const sailorData = {
+      sid: sailorID
+    };
+    // const boatData = {
+    //   bid: boatID
+    // };
+    // const reserveData = {
+    //   sid: reserveSailor,
+    //   bid: reserveBoat,
+    //   days: reserveDate
+    // }
+
+    //? Packaging data to be sent to backend based on searchParams.value
+    let body_value;
+    switch (searchParams.value) {
+      case "sailors":
+        body_value = JSON.stringify(sailorData);
+        break;
+      case "boats":
+        // body_value = JSON.stringify(boatData);
+        break;
+      case "reserves":
+        // body_value = JSON.stringify(reserveData);
+        break;
+
+      default:
+        console.log("Error in building body_value data!");
+    }
+
+    // console.log(sailorData)
+
+    fetch(`/api/delete/delete${searchParams.value}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: body_value,
+    });
+
+    setSailorID("");
+  };
+
   switch (searchParams.value) {
-    case 'sailors':
+    case "sailors":
       return (
         <form className="form">
           <div className="input-section">
             <label>Enter Sailor's id to be deleted</label>
-            <input className="input" type="number" required />
+            <input
+              className="input"
+              type="number"
+              required
+              value={sailorID}
+              onChange={(e) => handleChange(e, setSailorID)}
+            />
           </div>
 
-          <button type="submit" className="subm-btn">Delete</button>
+          <button type="submit" className="subm-btn" onClick={handleSubmit}>
+            Delete
+          </button>
         </form>
-      )      
+      );
 
-    case 'boats':
+    case "boats":
       return (
         <form className="form">
           <div className="input-section">
@@ -20,23 +85,25 @@ export default function Delete({ searchParams }) {
             <input className="input" type="number" required />
           </div>
 
-          <button type="submit" className="subm-btn">Delete</button>
+          <button type="submit" className="subm-btn">
+            Delete
+          </button>
         </form>
-      )      
+      );
 
-    case 'reserves':
+    case "reserves":
       return (
         <form className="form">
           <div className="flex gap-5">
             <div className="input-section items-end">
               <label>Enter Sailor id</label>
               <input className="input w-1/2" type="number" required />
-            </div>            
+            </div>
 
             <div className="input-section items-start">
               <label>Enter Boat id</label>
               <input className="input w-1/2" type="number" required />
-            </div>            
+            </div>
           </div>
 
           <div className="input-section">
@@ -44,17 +111,25 @@ export default function Delete({ searchParams }) {
             <input className="input" type="date" required />
           </div>
 
-          <button type="submit" className="subm-btn">Delete</button>
+          <button type="submit" className="subm-btn">
+            Delete
+          </button>
         </form>
-      )      
-  
+      );
+
     default:
-      return (
-        <p>Value error: {searchParams.value}</p>
-      )
+      return <p>Value error: {searchParams.value}</p>;
       break;
   }
 }
 
 //* Note:
 //? Kenapa hrs enter sid, bid, sama date nya? Karena problemnya adalah bisa jadi 1 orang mesen 1 boat yg sama untuk beberapa wktu yg berbeda, dan kita gada reserves_id, jd gbs delete specific. Kl di kasus gtu satu"nya yg bikin unik ya cmn si date nya. Kl ga brti nnti smua reserve si orang itu bakal ke-delete (takutnya)
+
+//* Even though you're only sending 1 data like sid only, you still have to make:
+//*   const sailorData = {
+//*       sid: sailorID
+//*     };
+//* Because:
+//! 1. JSON.stringify() is stringifying an object
+//! 2. The backend is expecting an sid, so if you don't package it in an object, you're sending only a number value (Ex: 0), but it's not expecting a number, it's expecting an sid with a value of a number instead
