e09afda0cf4512312e2d6c097290e62e20e52394
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2024-01-05 08:15:33 +0700

    /insert page finally worked!(for sailors only, apply to the rest next)

--- app/CRUD/insert/page.js
+++ app/CRUD/insert/page.js
@@ -1,4 +1,49 @@
+'use client'
+import { useState, useEffect } from 'react'
+
 export default function Insert({ searchParams }) {
+  const [sailorName, setSailorName] = useState("");
+  const [sailorRating, setSailorRating] = useState("");
+  const [sailorAge, setSailorAge] = useState("");
+  
+  const handleChange = (e, setStateFunc) => {
+    setStateFunc(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const sailorData = {
+      sname: sailorName,
+      rating: sailorRating,
+      age: sailorAge,
+    };
+
+    console.log(sailorData)
+
+    fetch(`/api/insert/insert${searchParams.value}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(sailorData)
+    })
+
+    setSailorName("");
+    setSailorRating("");
+    setSailorAge("");
+  }
+
+  //* Console log to test inserting values in input
+  // useEffect(() => {
+  //   console.log(sailorName)
+  // },[sailorName])
+  // useEffect(() => {
+  //   console.log(sailorRating)
+  // },[sailorRating])
+  // useEffect(() => {
+  //   console.log(sailorAge)
+  // },[sailorAge])
 
   switch (searchParams.value) {
     case 'sailors':
@@ -6,20 +51,39 @@ export default function Insert({ searchParams }) {
         <form className="form">
           <div className="input-section">
             <label>Insert Sailor's name</label>
-            <input className="input" type="text" required />
+            <input 
+              className="input"
+              type="text"
+              required
+              value={sailorName}
+              onChange={(e) => handleChange(e, setSailorName)}
+            />
           </div>
 
           <div className="input-section">
             <label>Insert Sailor's rating</label>
-            <input className="input" type="number" required min="1" max="10" />
+            <input 
+              className="input"
+              type="number"
+              required
+              min="1" max="10"
+              value={sailorRating}
+              onChange={(e) => handleChange(e, setSailorRating)}
+            />
           </div>
 
           <div className="input-section">
             <label>Insert Sailor's age</label>
-            <input className="input" type="number" min="20" max="80" required />
+            <input 
+              className="input"
+              type="number" min="20" max="80"
+              required
+              value={sailorAge}
+              onChange={(e) => handleChange(e, setSailorAge)}
+            />
           </div>
 
-          <button type="submit" className="subm-btn">Submit</button>
+          <button onClick={handleSubmit} type="submit" className="subm-btn">Submit</button>
         </form>
       )
 
@@ -28,15 +92,24 @@ export default function Insert({ searchParams }) {
         <form className="form">
           <div className="input-section">
             <label>Insert Boat name</label>
-            <input className="input" type="text" required />
+            <input 
+              className="input"
+              type="text"
+              required
+              
+            />
           </div>
 
           <div className="input-section">
             <label>Insert Boat color</label>
-            <input className="input" type="text" required />
+            <input 
+              className="input"
+              type="text"
+              required
+            />
           </div>
 
-          <button type="submit" className="subm-btn">Submit</button>
+          <button onCLick={handleSubmit} type="submit" className="subm-btn">Submit</button>
         </form>
       )
 
@@ -45,20 +118,35 @@ export default function Insert({ searchParams }) {
         <form className="form">
           <div className="input-section">
             <label>Insert Sailor's id</label>
-            <input className="input" type="number" required />
+            <input 
+              className="input"
+              type="number"
+              required
+            
+            />
           </div>
 
           <div className="input-section">
             <label>Insert Boat's id</label>
-            <input className="input" type="number" required />
+            <input 
+              className="input"
+              type="number"
+              required
+            
+            />
           </div>
 
           <div className="input-section">
             <label>Insert Reserve date</label>
-            <input className="input" type="date" required />
+            <input 
+              className="input"
+              type="date"
+              required
+            
+            />
           </div>
 
-          <button type="submit" className="subm-btn">Submit</button>
+          <button onCLick={handleSubmit} type="submit" className="subm-btn">Submit</button>
         </form>
       )
   
