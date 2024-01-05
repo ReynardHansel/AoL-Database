10fd5474042d17ffa5182b5a172579eda89acf7d
ReynardHansel <92787298+ReynardHansel@users.noreply.github.com>
2024-01-05 23:13:58 +0700

    Frontend to Backend for update sailors works

--- app/CRUD/update/sailors/page.js
+++ app/CRUD/update/sailors/page.js
@@ -1,4 +1,46 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function SailorUpdate() {
+  //* Variables to contain datas to be submitted to backend
+  const [sid, setSid] = useState("");
+  const [sName, setSName] = useState("");
+  const [rating, setRating] = useState("");
+  const [age, setAge] = useState("");
+
+  //*Triggered when user inputs something
+  const handleChange = (e, setStateFunc) => {
+    setStateFunc(e.target.value);
+  };
+
+  //* Triggered when clicking submit button
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    //? Variables to be submitted as JSON object to backend
+    const sailorData = {
+      sid: sid,
+      sname: sName,
+      rating: rating,
+      age: age,
+    };
+
+    // console.log(sailorData)
+
+    fetch(`/api/update/updatesailors`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(sailorData),
+    });
+
+    setSid("");
+    setSName("");
+    setRating("");
+    setAge("");
+  };
+
   return (
     <form className="form">
       <div className="input-section">
@@ -7,8 +49,8 @@ export default function SailorUpdate() {
           className="input"
           type="number"
           required
-          //   value={sailorName}
-          //   onChange={(e) => handleChange(e, setSailorName)}
+          value={sid}
+          onChange={(e) => handleChange(e, setSid)}
         />
       </div>
 
@@ -18,8 +60,8 @@ export default function SailorUpdate() {
           className="input"
           type="text"
           required
-          //   value={sailorName}
-          //   onChange={(e) => handleChange(e, setSailorName)}
+          value={sName}
+          onChange={(e) => handleChange(e, setSName)}
         />
       </div>
 
@@ -31,8 +73,8 @@ export default function SailorUpdate() {
           required
           min="1"
           max="10"
-          //   value={sailorRating}
-          //   onChange={(e) => handleChange(e, setSailorRating)}
+          value={rating}
+          onChange={(e) => handleChange(e, setRating)}
         />
       </div>
 
@@ -44,12 +86,12 @@ export default function SailorUpdate() {
           min="20"
           max="80"
           required
-          //   value={sailorAge}
-          //   onChange={(e) => handleChange(e, setSailorAge)}
+          value={age}
+          onChange={(e) => handleChange(e, setAge)}
         />
       </div>
 
-      <button type="submit" className="subm-btn">
+      <button onClick={handleSubmit} type="submit" className="subm-btn">
         Submit
       </button>
     </form>
