5fa9ea7ac068f7b678d6f74275f030a587e26a02
Gregorius <forvalem3@gmail.com>
2024-01-06 12:24:42 +0700

    delete apaini dah cape

--- app/api/delete/deleteboats/route.js
+++ app/api/delete/deleteboats/route.js
@@ -1,18 +1,53 @@
 import { db } from "../../../../database/server";
 import { NextResponse } from "next/server";
 
-export async function DELETE(req, res) {
+export async function DELETE(request) {
   try {
-    const { bid } = req.query;
-    const sql = `DELETE FROM boats WHERE bid = ${sid}`;
+    const { bid } = await request.json();
 
-    await db.query(sql, [bid], (err, result) => {
-      if (err) throw err;
+    const sql = `DELETE FROM boats WHERE sid = ${bid}`;
+
+    const data = await new Promise((resolve, reject) => {
+      db.query(sql, (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
+      });
     });
 
-    res.status(204).json({ message: "Boat deleted successfully" });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Error deleting boat" });
+    console.log(data);
+
+    if (data.affectedRows > 0) {
+      let json_response = {
+        status: "success",
+        message: "Boat deleted successfully",
+      };
+
+      return new NextResponse(JSON.stringify(json_response), {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    } 
+    
+    else {
+      let error_response = {
+        status: "error",
+        message: "Sailor not found",
+      };
+
+      return new NextResponse(JSON.stringify(error_response), {
+        status: 404,
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+  } catch (error) {
+    let error_response = {
+      status: "error",
+      message: error.message,
+    };
+
+    return new NextResponse(JSON.stringify(error_response), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
